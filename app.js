1ffacd2324045b8a48323fd3e3fe4cad41df054d
Robert Zavala <rbrtzavala@gmail.com>
2024-05-07 13:02:54 -0600

    create initial web components and import to app

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ import { loadData } from './services/Menu.js'
 import Router from './services/router.js'
 import Store from './services/store.js'
 
+// Web components
+import { MenuPage } from './components/MenuPage.js'
+import { DetailsPage } from './components/DetailsPage.js'
+import { OrderPage } from './components/OrderPage.js'
+
 window.app = {}
 app.store = Store
 app.router = Router
